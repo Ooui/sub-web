236aecd1f63e4a5edacfde60e7e1d89ec0b1f780
Ooui <70848437+Ooui@users.noreply.github.com>
2022-04-11 11:37:11 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -256,7 +256,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "sub-beta.vercel.app": "https://akashi-six.vercel.app/sub?",
+          "akashi-six.vercel.app": "https://akashi-six.vercel.app/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
